aac6ba30094a4d36c15f16d70e15baa2acb2ed41
Kevin Weidner <weidnerk@gmail.com>
2019-12-02 18:27:26 -0500

    Add OrderHistoryUpdateToList.

--- scrapeAPI/Controllers/ScraperController.cs
+++ scrapeAPI/Controllers/ScraperController.cs
@@ -194,6 +194,19 @@ namespace scrapeAPI.Controllers
             }
         }
 
+        [Route("updatetolist")]
+        [HttpPost]
+        public async Task<IHttpActionResult> OrderHistoryUpdateToList(OrderHistory oh)
+        {
+            string strCurrentUserId = User.Identity.GetUserId();
+            string connStr = ConfigurationManager.ConnectionStrings["OPWContext"].ConnectionString;
+            var settings = db.GetUserSettings(connStr, strCurrentUserId);
+
+            await db.OrderHistorySaveToList(oh);
+
+            return Ok();
+        }
+
         /// <summary>
         /// Get stored scan.
         /// This may be called on a timer to keep feeding results.
@@ -329,7 +342,7 @@ namespace scrapeAPI.Controllers
             string connStr = ConfigurationManager.ConnectionStrings["OPWContext"].ConnectionString;
             var settings = db.GetUserSettings(connStr, strCurrentUserId);
 
-            var mv = await FetchSeller.FillMatch(settings, rptNumber, minSold, daysBack, minPrice, maxPrice, activeStatusOnly, nonVariation, itemID);
+            var mv = await FetchSeller.FillMatch(settings, rptNumber, minSold, daysBack, minPrice, maxPrice, activeStatusOnly, nonVariation, itemID, 5);
             return Ok(mv);
         }
 
